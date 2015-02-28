17e2936f629d97e281cdf6b97a24e35a26e39714
paul de wouters <paul@hmn.md>
2015-02-28 19:40:33 +0000

    refactor to Cat Click Premium

--- cat-clicker/cat-click.js
+++ cat-clicker/cat-click.js
@@ -8,18 +8,51 @@
 	};
 
 	function addCat( cat ) {
-		document.getElementById( cat.id + "-name" ).innerHTML = cat.name;
-		document.getElementById( cat.id + "-img" ).src = cat.src;
+
+		var catMenu = document.querySelector('.cat-menu');
+		var cats = document.querySelector('.cats');
+
+		// Build menu
+		var menuItem = document.createElement("li");
+		var link = document.createElement("a");
+		link.textContent = cat.name;
+		link.href = "#" + cat.id;
+		link.id = cat.id;
+		menuItem.appendChild(link);
+		catMenu.appendChild(menuItem);
+
+		// Build main content
+		var catDiv = document.createElement("div");
+		cats.appendChild(catDiv);
+
+		var catImg = document.createElement("img");
+		catImg.id = cat.id + "-img";
+		catImg.src = cat.src;
+		catDiv.appendChild(catImg);
+
+		var catDesc = document.createElement("p");
+		var catName = document.createElement("span");
+		catName.textContent = cat.name + ' ';
+		catName.id = cat.id + "-name";
+		var catTally = document.createElement("span");
+		catTally.id = cat.id + "-tally";
+		catTally.textContent = cat.tally + ' likes.';
+		catDesc.appendChild(catName);
+		catDesc.appendChild(catTally);
+		catDiv.appendChild(catDesc);
+
 		document.getElementById( cat.id + "-img" ).addEventListener( 'click', function(){
 			cat.tally++;
-			document.getElementById( cat.id + "-tally" ).innerHTML = cat.tally;
+			document.getElementById( cat.id + "-tally" ).innerHTML = cat.tally + " likes";
 		}, false );
 	};
 
-	var cat1 = new Cat( "Garfield", "cat1", 'http://lorempixel.com/400/200/cats/1/' );
-	addCat(cat1);
+	var names = ['Garfield', 'Fritz', 'Boston', 'Phoenix', 'Lizzy', "Marmaduke"];
 
-	var cat2 = new Cat( "Fritz", "cat2", 'http://lorempixel.com/400/200/cats/4/' );
-	addCat(cat2);
+	for ( var i=0; i < names.length; i++ ) {
+		var cat = new Cat( names[i], names[i].toLowerCase(), "http://lorempixel.com/400/200/cats/" + i.toString() + "/" );
+		addCat( cat );
+
+	}
 
 })();
